29ab3661420aa4ac521446f19c7dc79fabfa5224
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2016-03-08 17:59:53 -0300

    avances en admin insc

--- main.php
+++ main.php
@@ -49,7 +49,7 @@ include( plugin_dir_path( __FILE__ ) . 'admin/sgcinsc_adminpage.php');
 
 include( plugin_dir_path( __FILE__ ) . 'admin/sgcinsc_adminoptions.php');
 
-include( plugin_dir_path( __FILE__ ) . 'admin/refoptions2.php');
+//include( plugin_dir_path( __FILE__ ) . 'admin/refoptions2.php');
 
 //Contenidos especiales
 include( plugin_dir_path( __FILE__ ) . 'sgcinsc_custom-content.php');
